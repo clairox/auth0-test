7a85a00fc1758553f4c28baa1961d8d6e0f3c8ff
Claire Q <claireq620@gmail.com>
2024-03-07 08:49:37 -0500

    feat: implement password changing in backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const { auth } = require('express-oauth2-jwt-bearer')
+const axios = require('axios')
+
+require('dotenv').config()
 
 const port = 8080
 const baseURL = 'http://127.0.0.1'
@@ -26,6 +29,34 @@ app.use(jwtCheck)
 app.get('/authorized', function (req, res) {
 	const { sub: uid } = req.auth.payload
 	res.json({ uid })
+})
+
+app.patch('/user/password', async function (req, res) {
+	const { sub: uid } = req.auth.payload
+	const { password } = req.body
+
+	const authHeader = `Bearer ${process.env.MGMT_API_TOKEN}`
+
+	axios
+		.patch(
+			`https://dev-ze6km2c7fqtpnwsy.us.auth0.com/api/v2/users/${uid}`,
+			{ password },
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					Authorization: authHeader,
+				},
+				data: JSON.stringify({ password }),
+			}
+		)
+		.then(response => {
+			return res.json(response.data)
+		})
+		.catch(err => {
+			const { statusCode, message } = err.response.data
+			return res.status(statusCode).json(message)
+		})
 })
 
 app.listen(port)
